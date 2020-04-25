4bbc874bf4b822a2b7a8d8d1599e6367b000a205
Spencer Apple <spencer@parkside.app>
2020-04-25 10:04:11 -0700

    Add z for directory navigation

--- .bashrc.shared.bash
+++ .bashrc.shared.bash
@@ -6,4 +6,8 @@ alias dotfiles="dgit reset --hard; ./copy-dotfiles.sh"
 export VISUAL=vim
 export EDITOR="$VISUAL"
 
+# install z which builds up a database of common directories
+# type `z dir-name` to jump to the directory name with autocomplete
+. ~/z/z.sh
+
 echo "Sourced .bashrc.shared.bash"
