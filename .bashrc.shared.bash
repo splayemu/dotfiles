da578dd2772f3e333cbb24b663ccd4e3d393cf47
Spencer Apple <spencer@parkside.app>
2020-04-25 10:33:04 -0700

    Move history into shared

--- .bashrc.shared.bash
+++ .bashrc.shared.bash
@@ -1,7 +1,16 @@
-
 # Use dgit to interact with our remote home repo stored in .dotfiles
 alias dgit='git --git-dir ~/.dotfiles/.git --work-tree=$HOME'
 alias dotfiles="dgit reset --hard; ./copy-dotfiles.sh"
+
+## Better History 
+# https://unix.stackexchange.com/questions/1288/preserve-bash-history-in-multiple-terminal-windows
+export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
+export HISTSIZE=100000                   # big big history
+export HISTFILESIZE=100000               # big big history
+shopt -s histappend                      # append to history, don't overwrite it
+# Save and reload the history after each command finishes
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+
 
 export VISUAL=vim
 export EDITOR="$VISUAL"
--- macos-dotfiles/.bashrc.macos.bash
+++ macos-dotfiles/.bashrc.macos.bash
@@ -1,15 +1,6 @@
 # 256 colors
 alias tmux="TERM=screen-256color-bce tmux"
 
-# history stuff found https://unix.stackexchange.com/questions/1288/preserve-bash-history-in-multiple-terminal-windows
-export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
-export HISTSIZE=100000                   # big big history
-export HISTFILESIZE=100000               # big big history
-shopt -s histappend                      # append to history, don't overwrite it
-
-# Save and reload the history after each command finishes
-export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
-
 # Add Homebrew `/usr/local/bin` and User `~/bin` to the `$PATH`
 PATH=/usr/local/bin:$PATH
 PATH=$HOME/bin:$PATH
